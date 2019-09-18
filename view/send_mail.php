456488997b9ef2a1dff0844e2c0e5b3080904b6a
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2019-09-18 13:58:04 +0200

    Update send_mail.php

--- view/send_mail.php
+++ view/send_mail.php
@@ -9,11 +9,11 @@
 require '../vendor/autoload.php';
 use Mailgun\Mailgun;
 
-$mg = Mailgun::create('f8295afcfc44ec0af88d344f45a8af3a-c27bf672-305d27b4'); // For EU servers
+$mg = Mailgun::create('API-KEY'); // For EU servers
 
 // Now, compose and send your message.
 // $mg->messages()->send($domain, $params);
-$mg->messages()->send('sandbox06d4d0f9aa3e4b88a0c839aeba471302.mailgun.org', [
+$mg->messages()->send('DOMAIN', [
 	'from'    => 'caspar.brenneisen@pbs.ch',
 	'to'      => $_POST[mailaddress],
 	'subject' => 'MiData Adresse',
